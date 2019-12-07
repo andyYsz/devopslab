e8b756fbb83177197b589ca203f818551b147caa
ysz <bjyszyan@cn.ibm.com>
2019-12-07 14:17:41 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
-export GITACCOUNT=
+export GITACCOUNT=andyYsz
 
-export MYCLUSTER=
-export KUBECONFIG=
+export MYCLUSTER=tektonknativebeijing26
+export KUBECONFIG=export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing26/kube-config-tok02-tektonknativebeijing26.yml
 
-export NAMESPACE=
-export EMAIL=
-export INGRESS=
+export NAMESPACE=tektondevops-tesla2019
+export EMAIL=572670160@qq.com
+export INGRESS=tektonknativebeijing26-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
